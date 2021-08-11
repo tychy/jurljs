7f7274a725da0885b05b2df0b9ee014ff9e33f0b
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-08-12 00:03:23 +0900

    3.4までの実装

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	polygon(w)
+	//lissajous(w)
+}
