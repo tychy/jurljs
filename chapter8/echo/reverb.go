c65feaf87da56f0f646020f82adb6d72bfe9174d
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-09-24 16:44:06 +0900

    goroutineを使った練習を追加

---
+++ chapter8/echo/reverb.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+var port = flag.String("p", "8000", "port")
+
+func main() {
+	flag.Parse()
+	listener, err := net.Listen("tcp", "localhost:"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	time.Sleep(delay)
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
